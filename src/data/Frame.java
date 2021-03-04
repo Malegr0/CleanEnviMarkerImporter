9e83f3a3e7237fc0cbdd6c6586740fe49b3bf7a8
Malegr0 <schoening.andreas@gmx.de>
2021-03-04 18:22:45 +0100

    added frame components

--- src/data/Frame.java
+++ src/data/Frame.java
@@ -1,6 +1,7 @@
 package data;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame {
 
@@ -9,10 +10,40 @@ public class Frame {
         JPanel panel = new JPanel();
 
         //create components
+        JLabel longitudeLabel = new JLabel("Longitude:");
+        JTextField longitudeTextField = new JTextField();
+        longitudeTextField.setPreferredSize(new Dimension(100, longitudeTextField.getPreferredSize().height));
 
-        //adding components to panel
+        JLabel latituteLabel = new JLabel("Latitute:");
+        JTextField latituteTextField = new JTextField();
+        latituteTextField.setPreferredSize(new Dimension(100, latituteTextField.getPreferredSize().height));
+
+        JLabel nameLabel = new JLabel("Name:");
+        JTextField nameTextField = new JTextField();
+        nameTextField.setPreferredSize(new Dimension(100, nameTextField.getPreferredSize().height));
+
+        JLabel addressLabel = new JLabel("Address:");
+        JTextField addressTextField = new JTextField();
+        addressTextField.setPreferredSize(new Dimension(200, addressTextField.getPreferredSize().height));
+
+        JButton sendingButton = new JButton("Send data!");
 
         //button listener logic
+
+        sendingButton.addActionListener(e -> {
+
+        });
+
+        //adding components to panel
+        panel.add(longitudeLabel);
+        panel.add(longitudeTextField);
+        panel.add(latituteLabel);
+        panel.add(latituteTextField);
+        panel.add(nameLabel);
+        panel.add(nameTextField);
+        panel.add(addressLabel);
+        panel.add(addressTextField);
+        panel.add(sendingButton);
 
         //frame settings
         frame.add(panel);
