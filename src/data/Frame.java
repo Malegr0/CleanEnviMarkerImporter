ede26a0d482121accd174dc61911f0a625d48800
Malegr0 <schoening.andreas@gmx.de>
2021-03-04 17:43:55 +0100

    added frame

--- src/data/Frame.java
+++ src/data/Frame.java
@@ -1,4 +1,25 @@
 package data;
 
+import javax.swing.*;
+
 public class Frame {
+
+    Frame() {
+        JFrame frame = new JFrame("Send Data to Markers Database");
+        JPanel panel = new JPanel();
+
+        //create components
+
+        //adding components to panel
+
+        //button listener logic
+
+        //frame settings
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
 }
